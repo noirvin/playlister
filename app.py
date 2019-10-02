c2fcae748eb75e2e92385e105a53bc0c25af5ff0
noirvin <vin.sp97@googlemail.com>
2019-10-01 21:03:01 -0700

    made the playlist index route

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    """Return homepage."""
-    return 'Hello, world!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+
+@app.route('/')
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists)
